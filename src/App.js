37224bad2a2e4d7594657e05591a05acea084ded
aytaconturk <aytconturk@gmail.com>
2022-10-13 10:35:47 +0300

    first init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./_components/Header";
+import Switch from "./_components/Switch";
+//import scss from './_scss/main.scss'
+import './_scss/main.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Switch/>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
